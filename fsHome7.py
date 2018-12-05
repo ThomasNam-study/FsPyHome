bebe5f9868a57ebf9ccc582c9b6e9b4f32ef5c5a
RED <purred@udid.co.kr>
2018-12-05 20:18:46 +0900

    7장 2번째 과제 시작

--- fsHome7.py
+++ fsHome7.py
@@ -16,4 +16,63 @@
 # 6. __gt__ : > 연산자를 구현할 수 있다.
 # 7. __ge__ : >= 연산자를 구현할 수 있다.
 
+class Wallet:
+    money = 0
+
+    # 생성자
+    def __init__(self, name):
+        self.owner = name
+
+    def __str__(self):
+        return "{}의 지갑입니다.".format(self.owner)
+
+    def __repr__(self):
+        return "{}의 지갑입니다.".format(self.owner)
+
+    def print_owner_name(self):
+        print('owner name is ', self.owner)
+
+    def print_now_money(self):
+        print("현재 잔액 : ", self.money)
+
+    def spend(self, m):
+        if self.money < m:
+            print("돈이 부족합니다.")
+        else:
+            self.money -= m
+            print("{}를 지출했습니다.".format(m))
+
+        self.print_now_money()
+
+    def income(self, m):
+        self.money += m
+        self.print_now_money()
+
+
+class Account(Wallet):
+
+    def __init__(self, name, account_number):
+        super().__init__(name)
+        self.account_number = account_number
+
+    def __str__(self):
+        return "{}의 계좌입니다. 계좌번호 : {}".format(self.owner, self.account_number)
+
+    def __add__(self, other):
+        return self.money + other.money
+
+    # 인스턴스를 직접 호출한다.
+    def __call__(self, *args, **kwargs):
+        print("호출되었습니다")
+
+    def print_account_number(self):
+        print("계좌 번호는 {} 입니다.".format(self.account_number))
+
+    def send_money(self, money, to):
+        if self.money > money:
+            to.money += money
+            self.money -= money
+            print("{}원을 {}에게 보냈습니다.".format(money, to.owner))
+            self.print_now_money()
+
 
